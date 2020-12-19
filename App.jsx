3d32d856e8bdbf4b5476dec75efb9abc9f19c818
Jason Addleman <jas7457@gmail.com>
2020-12-18 23:39:17 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./App.scss";
+
+export default function App() {
+  return <div className="app bg-red-800 p-8 mt-12">Hi</div>;
+}
---
+++ snowpack.config.js
@@ -0,0 +1,25 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: ["@snowpack/plugin-sass", "@snowpack/plugin-postcss"],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
